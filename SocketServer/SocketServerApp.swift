3aace907b900e56ea0ac16917cc88c928f548542
Jorge Mendoza <jorgevmendoza@gmail.com>
2024-12-19 21:33:31 +0100

    Initial Commit

---
+++ SocketServer/SocketServerApp.swift
@@ -0,0 +1,17 @@
+//
+//  SocketServerApp.swift
+//  SocketServer
+//
+//  Created by Jorge Mendoza on 12/19/24.
+//
+
+import SwiftUI
+
+@main
+struct SocketServerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
